b309bb61ad1662979f0bdba15402b49dd76dc7ec
Swadhin Ghosh <167126803+swadhin-in@users.noreply.github.com>
2025-12-14 19:26:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,10 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("DB Connected"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// app.listen(5000, () => console.log("Server running on port 5000"));
+
+const PORT = process.env.PORT || 5000; // Use environment variable
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // 1. REGISTER (Run this once via Postman/Curl to create your admin, then delete/comment out)
 app.post('/api/register', async (req, res) => {
